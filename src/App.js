42d00c4ab1f081f22ae1495cf6afe351cfc44fd6
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-08 01:44:04 +0530

    Structure of the app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>Currency Converter</div>
+      <div className="card card-body">
+        <h5>1 USD is equivalent to</h5>
+        <h2>1.87 EUR</h2>
+        <p>As of 2019/1/2</p>
+        <div className="row">
+          <div className="col-sm-10">
+            <form className="form-inline mb-4">
+              <input className="form-control form-control-lg mx-3" />
+              <select className="form-control form-control-lg">
+                <option>Option 1</option>
+              </select>
+            </form>
+            <form className="form-inline mb-4">
+              <input className="form-control form-control-lg mx-3" />
+              <select className="form-control form-control-lg">
+                <option>Option 1</option>
+              </select>
+            </form>
+          </div>
+
+          <div className="col-sm-2 align-self-center">
+            <h1 className="swap">&#8595;&#8593;</h1>
+          </div>
+        </div>
+      </div>
     );
   }
 }
